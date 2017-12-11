1e44e5a99336dc31d0ffa7c0b55dfd7b61e34d6f
starrygit <kenvpn@gmail.com>
2017-12-11 11:34:37 +0800

    no message

--- x13.c
+++ x13.c
@@ -4,21 +4,28 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "sha3/sph_blake.h"
 #include "sha3/sph_sm3.h"
 
 
 void sm3_hash(const char* input, char* output)
 {
+    sph_blake512_context     ctx_blake;
     sm3_ctx_t               ctx_sm3;
 
     //these uint512 in the c++ source of the client are backed by an array of uint32
-    uint32_t hashA[16];
+    uint32_t hashA[16], hashB[16];
 
+    sph_blake512_init(&ctx_blake);
+    sph_blake512 (&ctx_blake, input, 80);
+    sph_blake512_close (&ctx_blake, hashA);
+
+    memset(hashB, 0, 64);
     sm3_init(&ctx_sm3);
-    sph_sm3(&ctx_sm3, input, 80);
-    sph_sm3_close(&ctx_sm3, hashA);
+    sph_sm3(&ctx_sm3, hashA, 64);
+    sph_sm3_close(&ctx_sm3, hashB);
 
-    memcpy(output, hashA, 32);
+    memcpy(output, hashB, 32);
 	
 }
 
