3354062f0e4fe39304061ae80b0013cf5062da43
starrygit <kenvpn@gmail.com>
2017-12-11 11:40:32 +0800

    no message

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup, Extension
 sm3_hash_module = Extension('sm3_hash',
                                sources = ['x13module.c',
                                           'x13.c',
+                                          'sha3/blake.c',
 										  'sha3/sm3.c',],
 
                                include_dirs=['.', './sha3'])
